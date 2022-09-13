6648b27da01265f33b67955d0621b9978fb9acf3
gabizsiros <gabi.zsiros@gmail.com>
2022-09-13 22:40:03 +0200

    updating math4.jpg, changing shell script name

--- exam-out.sh
+++ exam-out.sh
@@ -1,4 +1,4 @@
 mkdir ~/made_by_my_script
-echo "Hello World!" > ~/made_by_my_script/hello.txt
+echo "Hello world" > ~/made_by_my_script/hello.txt
 ls -f ~/made_by_my_script/ > ~/made_by_my_script/list_of_files.txt
 grep -w -i hello ~/made_by_my_script/list_of_files.txt > ~/made_by_my_script/grep_results.txt
